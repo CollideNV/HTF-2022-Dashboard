5183d1788aebbf11a56a6415197bd8ce2174fddd
Gwen S. J. Nijssen <gwen.nijssen@collide.be>
2022-09-26 10:01:03 +0200

    Implement base style

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,36 +1,23 @@
-import { Button } from '@mui/material'
 import { FC } from 'react'
-import DateDisplay from '../../components/DataDisplay/DateDisplay'
 import environment from '../../resources/constants/environment'
 import styles from './HomePage.module.scss'
+
+import sigil_1 from '../../resources/assets/sigil_1.png'
 
 const baseUrl = environment.api.url
 
 const HomePage: FC = () => {
     return (
         <div className={styles.HomePage}>
-            <h1>Hello world!</h1>
-            <DateDisplay />
-            <h5>Environment Variable:</h5>
-            {baseUrl ? (
-                <p>{`>>>>>${baseUrl}<<<<<`}</p>
-            ) : (
-                <>
-                    <p>
-                        The environment variable <span style={{ fontWeight: 'bold' }}>REACT_APP_API_URL</span> is empty or the .env file has not yet been made.
-                    </p>
-                    <p>
-                        Be sure to make a<span style={{ fontWeight: 'bold' }}>.env.local</span> file
-                    </p>
-                </>
-            )}
-            <button>default button</button>
-            <Button>mui button</Button>
-            <Button color="primary">mui primary button</Button>
-            <Button color="secondary">mui secondary button</Button>
-            <Button color="success">mui success button</Button>
-            <Button color="warning">mui warning button</Button>
-            <Button color="error">mui error button</Button>
+            <div className={styles.container}>
+                <div className={styles.navigation}>
+                    <h2>Deadline - Hack The Future:</h2>
+                    <p className={styles.clock}>04:36:45</p>
+                    <img src={sigil_1} className={styles.sigil} />
+                </div>
+                <div className={styles.body}></div>
+                <div className={styles.leather} />
+            </div>
         </div>
     )
 }
