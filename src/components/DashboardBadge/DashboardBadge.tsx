d2caf36a72b0dd0c40d43f802f45f93c0bb9ca37
Gwen S. J. Nijssen <gwen.nijssen@collide.be>
2022-09-28 18:33:50 +0200

    Init dummy data

---
+++ src/components/DashboardBadge/DashboardBadge.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react'
+import CyanBadge from '../../resources/assets/badges/cyan_badge.min.svg'
+import EmptyBadge from '../../resources/assets/badges/empty_badge.min.svg'
+import FailBadge from '../../resources/assets/badges/fail_badge.min.svg'
+import LimeBadge from '../../resources/assets/badges/lime_badge.min.svg'
+import ThunderBadge from '../../resources/assets/badges/thunder_badge.min.svg'
+import { BadgeType } from '../../types/BadgeType'
+import styles from './DashboardBadge.module.scss'
+
+interface DashboardBadgeProps {
+    'data-testid'?: string
+    type?: BadgeType
+}
+
+const DashboardBadge: FC<DashboardBadgeProps> = ({ 'data-testid': dataTestId = 'DashboardBadge', type = BadgeType.EMPTY_BADGE }) => {
+    const renderBadge = (badgeType: BadgeType) => {
+        switch (badgeType) {
+            case BadgeType.THUNDER_BADGE:
+                return ThunderBadge
+            case BadgeType.CYAN_BADGE:
+                return CyanBadge
+            case BadgeType.LIME_BADGE:
+                return LimeBadge
+            case BadgeType.FAIL_BADGE:
+                return FailBadge
+            default:
+                return EmptyBadge
+        }
+    }
+
+    return (
+        <div className={styles.DashboardBadge} data-testid={dataTestId}>
+            <img src={renderBadge(type)} className={styles.svgBadge} />
+        </div>
+    )
+}
+
+export default DashboardBadge
--- src/components/DashboardTable/DashboardTable.tsx
+++ src/components/DashboardTable/DashboardTable.tsx
@@ -1,10 +1,135 @@
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import { FC, useCallback, useMemo } from 'react'
+import { BadgeType } from '../../types/BadgeType'
+import DashboardBadge from '../DashboardBadge/DashboardBadge'
 import styles from './DashboardTable.module.scss'
 
 interface DashboardTableProps {
     'data-testid'?: string
 }
+
+interface Spell {
+    type: BadgeType
+    solved: boolean
+    remainingAttempts: number
+}
+
+interface Problem {
+    // badgeUrl: string
+    // description: string
+    // name: string
+    // score: number
+    // solved: boolean
+    spells: Spell[]
+}
+
+interface Team {
+    name: string
+    spells: Spell[]
+    score: number
+}
+
+const teams: Team[] = [
+    {
+        name: 'Team A',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: true, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: true, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: true, remainingAttempts: 2 }
+        ],
+        score: 4800
+    },
+    {
+        name: 'Team B',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: false, remainingAttempts: 2 }
+        ],
+        score: 0
+    },
+    {
+        name: 'Team C',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: true, remainingAttempts: 0 },
+            { type: BadgeType.LIME_BADGE, solved: true, remainingAttempts: 0 },
+            { type: BadgeType.THUNDER_BADGE, solved: true, remainingAttempts: 0 }
+        ],
+        score: 4950
+    },
+    {
+        name: 'Team D',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: false, remainingAttempts: 0 },
+            { type: BadgeType.LIME_BADGE, solved: false, remainingAttempts: 0 },
+            { type: BadgeType.THUNDER_BADGE, solved: false, remainingAttempts: 0 }
+        ],
+        score: 0
+    },
+    {
+        name: 'Team E',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: true, remainingAttempts: 0 },
+            { type: BadgeType.LIME_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: true, remainingAttempts: 2 }
+        ],
+        score: 3300
+    },
+    {
+        name: 'Team F',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: true, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: false, remainingAttempts: 0 }
+        ],
+        score: 1440
+    },
+    {
+        name: 'Team G',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: true, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: false, remainingAttempts: 0 },
+            { type: BadgeType.THUNDER_BADGE, solved: true, remainingAttempts: 2 }
+        ],
+        score: 3240
+    },
+    {
+        name: 'Team H',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: false, remainingAttempts: 2 }
+        ],
+        score: 0
+    },
+    {
+        name: 'Team I',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: true, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: false, remainingAttempts: 2 }
+        ],
+        score: 1500
+    },
+    {
+        name: 'Team J',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: false, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: true, remainingAttempts: 2 }
+        ],
+        score: 3000
+    },
+    {
+        name: 'Team K',
+        spells: [
+            { type: BadgeType.CYAN_BADGE, solved: true, remainingAttempts: 2 },
+            { type: BadgeType.LIME_BADGE, solved: true, remainingAttempts: 2 },
+            { type: BadgeType.THUNDER_BADGE, solved: true, remainingAttempts: 2 }
+        ],
+        score: 500
+    }
+]
 
 const headerColumns = ['Rank', 'Team', 'Problem 1', 'Problem 2', 'Problem 3', 'Total Score']
 
@@ -17,33 +142,47 @@ const DashboardTable: FC<DashboardTableProps> = ({ 'data-testid': dataTestId = '
         ))
     }
 
-    const renderBadges = () => {
-        return (
-            <div className={styles.badge_container}>
-                <div className={styles.empty_badge}></div>
-                <div className={styles.empty_badge}></div>
-                <div className={styles.empty_badge}></div>
-            </div>
-        )
-    }
+    const renderBadges = useCallback((spells: Spell[]) => {
+        const renderBadge = (spell: Spell) => {
+            let badge: BadgeType = BadgeType.EMPTY_BADGE
+            if (spell.solved) {
+                badge = spell.type
+            } else {
+                badge = spell.remainingAttempts > 0 ? BadgeType.EMPTY_BADGE : BadgeType.FAIL_BADGE
+            }
 
-    const RenderTableRow: FC<{ index: number }> = useCallback(({ index }) => {
-        return (
-            <TableRow>
-                <TableCell className={styles.tableCell}>{index + 1}.</TableCell>
-                <TableCell className={styles.tableCell}>Team A</TableCell>
-                <TableCell className={styles.tableCell}>{renderBadges()}</TableCell>
-                <TableCell className={styles.tableCell}>{renderBadges()}</TableCell>
-                <TableCell className={styles.tableCell}>{renderBadges()}</TableCell>
-                <TableCell className={styles.tableCell}>10000</TableCell>
-            </TableRow>
-        )
+            return <DashboardBadge type={badge} />
+        }
+
+        return <div className={styles.badge_container}>{spells.map((s) => renderBadge(s))}</div>
     }, [])
 
+    const RenderTableRow: FC<{ index: number; team: Team }> = useCallback(
+        ({ index, team }) => {
+            return (
+                <TableRow>
+                    <TableCell className={styles.tableCell}>{index + 1}.</TableCell>
+                    <TableCell className={styles.tableCell}>{team.name}</TableCell>
+                    {team.spells.map((spell, i) => {
+                        return (
+                            <TableCell key={i} className={styles.badgesCell}>
+                                {renderBadges(team.spells)}
+                            </TableCell>
+                        )
+                    })}
+                    <TableCell className={styles.tableCell}>{team.score}</TableCell>
+                </TableRow>
+            )
+        },
+        [renderBadges]
+    )
+
     const renderTableRows = useMemo(() => {
+        const sortedTeams = teams.sort((a, b) => b.score - a.score)
         const tableRows = []
-        for (let index = 0; index < 20; index++) {
-            tableRows.push(<RenderTableRow index={index} key={index} />)
+
+        for (let index = 0; index < sortedTeams.length; index++) {
+            tableRows.push(<RenderTableRow index={index} team={sortedTeams[index]} key={index} />)
         }
 
         return <>{tableRows}</>
