6e7ca7a3848d88010a56c6d63f9d5c161456691c
Gwen S. J. Nijssen <gwen.nijssen@collide.be>
2022-10-12 15:03:41 +0200

    Init Countdown component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { FC, useMemo } from 'react'
 import styles from './HomePage.module.scss'
 
 import { CircularProgress } from '@mui/material'
+import Countdown from '../../components/Countdown/Countdown'
 import DashboardTable from '../../components/DashboardTable/DashboardTable'
 import { useGetDashboardQuery } from '../../redux/services/dashboardApi'
 import sigil_1 from '../../resources/assets/sigil_1.png'
@@ -28,6 +29,7 @@ const HomePage: FC = () => {
                     <div className={styles.navigation}>
                         <h2>Deadline - Hack The Future:</h2>
                         <div className={styles.clock}>04:36:45</div>
+                        <Countdown />
                     </div>
                     {renderedBody}
                 </div>
