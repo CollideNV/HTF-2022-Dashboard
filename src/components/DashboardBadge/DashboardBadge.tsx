634ae163edc27af90a2c6530d6135f082b869ef4
Gwen S. J. Nijssen <gwen.nijssen@collide.be>
2022-10-13 14:25:48 +0200

    Add impact badge

--- src/components/DashboardBadge/DashboardBadge.tsx
+++ src/components/DashboardBadge/DashboardBadge.tsx
@@ -2,6 +2,7 @@ import DroughtBadge from '../../resources/assets/badges/drought_badge.min.svg'
 import EmptyBadge from '../../resources/assets/badges/empty_badge.min.svg'
 import FailBadge from '../../resources/assets/badges/fail_badge.min.svg'
 import FloodBadge from '../../resources/assets/badges/flood_badge.min.svg'
+import ImpactBadge from '../../resources/assets/badges/impact_badge.min.svg'
 import SuccessBadge from '../../resources/assets/badges/success_badge.min.svg'
 import { BadgeType } from '../../types/BadgeType'
 import styles from './DashboardBadge.module.scss'
@@ -17,6 +18,8 @@ const DashboardBadge = ({
 }: DashboardBadgeProps) => {
     const renderBadge = (badgeType: BadgeType) => {
         switch (badgeType) {
+            case BadgeType.IMPACT_BADGE:
+                return ImpactBadge
             case BadgeType.FLOOD_BADGE:
                 return FloodBadge
             case BadgeType.DROUGHT_BADGE:
