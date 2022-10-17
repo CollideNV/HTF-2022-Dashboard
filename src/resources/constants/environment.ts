bf3cff26d487c60d1d2e5f7202d67aa3d141a669
Gwen S. J. Nijssen <gwen.nijssen@collide.be>
2022-10-17 11:34:16 +0200

    Update src/resources/constants/environment.ts
    Co-authored-by: jonas-collide <111110705+jonas-collide@users.noreply.github.com>

--- src/resources/constants/environment.ts
+++ src/resources/constants/environment.ts
@@ -4,5 +4,5 @@ export default {
     dashboard_api: {
         url: REACT_APP_DASHBOARD_API_URL!
     },
-    deadline: REACT_APP_DEADLINE ? REACT_APP_DEADLINE : '2022-11-15T16:00:00'
+    deadline: REACT_APP_DEADLINE ?? '2022-11-15T16:00:00'
 }
